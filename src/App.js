14200761eae5a897471a1b479345e74181472527
MR.R <razor.wu1994@gmail.com>
2019-03-10 21:22:05 -0400

    set

--- src/App.js
+++ src/App.js
@@ -1,32 +1,73 @@
 import React from "react";
 import "./styles.css";
-import Results from "./components/Results";
-import Row from "./components/Row";
+
 import Button from "./components/Button";
 import Pool from "./components/Pool";
+
+import left from "./images/left-6.svg";
+import right from "./images/right-6.svg";
+
+const SPACE_KEY = 32;
+
 class App extends React.Component {
   state = {
-    mode: "vertical"
+    ordinal: [0, 0],
+    postConfig: [7, 7],
+    active: 0
   };
+  componentDidMount() {
+    setInterval(() => {
+      this.setState((state, props) => {
+        let ordinal = state.ordinal;
+        ordinal[state.active] =
+          ordinal[state.active] < 7 ? ordinal[state.active] + 1 : 0;
+        return {
+          ordinal: ordinal
+        };
+      });
+    }, 1000);
+    document.addEventListener("keydown", this._handleKeyDown);
+  }
+  // shouldComponentUpdate(nextProps, nextState) {
+  //   // return (
+  //   //   nextState.ordinal[this.state.active] !==
+  //   //   this.state.ordinal[this.state.active]
+  //   // );
+  // }
+
+  _handleKeyDown = event => {
+    event.preventDefault();
+    switch (event.keyCode) {
+      case SPACE_KEY:
+        this.setState((state, props) => ({
+          active: state.active < 1 ? state.active + 1 : 0
+        }));
+        break;
+      default:
+        break;
+    }
+  };
+
   render() {
     return (
       <div className="grid-container">
         <div>
-          <div className="verticalCentralDiv">
-            <Button
-              label={"switch mode"}
-              className="modeController"
-              onClick={() =>
-                this.setState((state, props) => ({
-                  mode: state.mode === "vertical" ? "horizontal" : "vertical"
-                }))
-              }
-            />
-            <h4>Current Mode: Vertical</h4>
-          </div>
+          <div className="verticalCentralDiv" />
         </div>
         <div className="marginDiv">
-          <Pool mode={this.state.mode} />{" "}
+          <Pool
+            poolData={[
+              { img: left, pos: "left" },
+              { img: right, pos: "right" }
+            ]}
+            ordinal={this.state.ordinal}
+            speed={1000}
+            posConfig={this.state.postConfig}
+            active={this.state.active}
+          />{" "}
+          <div style={{ textAlign: "center" }}>
+            <h4>Press Space to Select Character</h4>
+          </div>
         </div>
         <div />
       </div>
--- src/components/Pool.js
+++ src/components/Pool.js
@@ -1,25 +1,40 @@
 import React from "react";
 import "./styles.css";
-import left from "../images/left.svg";
-import right from "../images/right.svg";
 
-export default function Pool({ mode }) {
-  switch (mode) {
-    case "vertical":
-      return (
-        <div className="grid-2-columns">
-          <div className="grid-col">{left}</div>
-          <div className="grid-col">{right}</div>
-        </div>
-      );
-    case "horizontal":
-    default:
-      return (
-        <div className="grid-3-rows">
-          <div>Row1</div>
-          <div>Row2</div>
-          <div>Row3</div>
-        </div>
-      );
+export default class Pool extends React.Component {
+  render() {
+    const { poolData, speed, ordinal, posConfig, active } = this.props;
+
+    return (
+      <div className="grid-2-columns">
+        {(() =>
+          poolData.map((pool, idx) => {
+            let animationConfig =
+              ordinal[active] <= posConfig[idx]
+                ? {
+                    animationName: `${pool.pos}-col-${ordinal[idx]}`,
+                    animationDuration: `${speed || "2000"}ms`
+                  }
+                : {};
+            if (idx !== active) {
+              animationConfig = {
+                ...animationConfig,
+                animationPlayState: "paused"
+              };
+            }
+            return (
+              <div
+                className={`grid-col ${pool.pos} ${
+                  active === idx ? "active" : "inactive"
+                }`}
+                style={{
+                  backgroundImage: `url(${pool.img})`,
+                  ...animationConfig
+                }}
+              />
+            );
+          }))()}
+      </div>
+    );
   }
 }
