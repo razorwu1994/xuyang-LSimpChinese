6f410ea2fa228f0377e5ce034855f098a8338578
razorwu1994 <razor.wu1994@gmail.com>
2019-03-14 11:39:54 -0400

    Updates

---
+++ precache-manifest.281d4be8a8bac04d77f26c31faa4953e.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "c48cd0686a846436390460834a90a2b3",
+    "url": "/xuyang-LSimpChinese/static/media/tian.c48cd068.jpg"
+  },
+  {
+    "revision": "846c6eecd789ac2fc652935646970358",
+    "url": "/xuyang-LSimpChinese/static/media/right-6.846c6eec.svg"
+  },
+  {
+    "revision": "cd0bc049c89826c08535191819df914e",
+    "url": "/xuyang-LSimpChinese/static/media/left-6.cd0bc049.svg"
+  },
+  {
+    "revision": "a30cc2fe86bfa49b83c3a2d1b6ecf2da",
+    "url": "/xuyang-LSimpChinese/static/media/background.a30cc2fe.jpg"
+  },
+  {
+    "revision": "dd42ef80cd16568b7632",
+    "url": "/xuyang-LSimpChinese/static/js/runtime~main.c7ad4f2e.js"
+  },
+  {
+    "revision": "1710b6f1efc34465f654",
+    "url": "/xuyang-LSimpChinese/static/js/main.b6851eb5.chunk.js"
+  },
+  {
+    "revision": "319a4eb3f665cf31b943",
+    "url": "/xuyang-LSimpChinese/static/js/2.2322160f.chunk.js"
+  },
+  {
+    "revision": "1710b6f1efc34465f654",
+    "url": "/xuyang-LSimpChinese/static/css/main.928598e9.chunk.css"
+  },
+  {
+    "revision": "ed031abd878e077a59da74d6837934b4",
+    "url": "/xuyang-LSimpChinese/index.html"
+  }
+];
