9e474b1400aa6e1f5a7cfb4a895d317501b0fc19
razorwu1994 <razor.wu1994@gmail.com>
2019-04-09 08:17:05 -0400

    g

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
+import { HashRouter as Router, Route, Link, Switch } from "react-router-dom";
 import createHistory from "history/createBrowserHistory";
 
 import { ButtonGroup, Button, Row, Col } from "react-bootstrap";
